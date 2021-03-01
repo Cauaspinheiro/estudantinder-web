d3c573f7eb3f5bf0ea3f46e43adba2d949a6df41
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-03-01 19:11:26 -0300

    feat: adiciona estrutura basica para build do tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   purge: {
-    enabled: true,
+    enabled: process.env.NODE_ENV === 'prod',
     content: ['src/*.html']
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      bg_page: "#fbfbfb",
+      green: "#0FAD58",
+      purple: "#5B14F5",
+      light_purple: "#F3EDFF",
+    }
   },
   variants: {
     extend: {},
