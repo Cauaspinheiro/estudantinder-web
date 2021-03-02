7bf3c29448af5b359bfade4d1f37720ac14b60f8
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2021-03-02 00:49:57 -0300

    feat: adiciona primeira página do app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'prod',
-    content: ['src/*.html']
+    content: ['src/*.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      bg_page: "#fbfbfb",
-      green: "#0FAD58",
-      purple: "#5B14F5",
-      light_purple: "#F3EDFF",
-    }
+    extend: {
+      colors: {
+        'bg-page': '#fbfbfb',
+        'primary-green': '#0FAD58',
+        'primary-purple': '#5B14F5',
+        'light-purple': '#F3EDFF',
+      },
+      fontFamily: {
+        poppins: 'Poppins, Roboto, sans-serif',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
